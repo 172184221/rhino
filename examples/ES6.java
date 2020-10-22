c1af2a211853d1be489d8d6cb8c25c2df5794116
gangwangn <gangwangn@isoftstone.com>
2020-10-22 10:46:05 +0800

    配置国内镜像仓库

--- examples/ES6.java
+++ examples/ES6.java
@@ -5,9 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Function;
 import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
 
 /**
  * Example of controlling the JavaScript execution engine.
@@ -15,7 +13,7 @@ import org.mozilla.javascript.ScriptableObject;
  * We evaluate a script and then manipulate the result.
  *
  */
-public class Control {
+public class ES6 {
 
     /**
      * Main entry point.
@@ -32,7 +30,7 @@ public class Control {
             // Set version to JavaScript1.2 so that we get object-literal style
             // printing instead of "[object Object]"
 //            cx.setLanguageVersion(Context.VERSION_1_2);
-            cx.setLanguageVersion(Context.VERSION_1_8);
+            cx.setLanguageVersion(Context.VERSION_ES6);
 
             // Initialize the standard objects (Object, Function, etc.)
             // This must be done before scripts can be executed.
@@ -40,30 +38,14 @@ public class Control {
 
             // Now we can evaluate a script. Let's create a new object
             // using the object literal notation.
-            Object result = cx.evaluateString(scope, "obj = {a:1, b:['x','y']}",
+            String templateES6="let p1=2222222;let b=`There are <b>${p1}</b> !`;function f(){return b;};f();";
+
+            //let p1=2222222;let b=`There are <b>${p1}</b> !`;function f(){return b;};f();
+
+            Object result = cx.evaluateString(scope, templateES6,
                                               "MySource", 1, null);
+            System.out.println("=========="+result);
 
-            Scriptable obj = (Scriptable) scope.get("obj", scope);
-
-            // Should print "obj == result" (Since the result of an assignment
-            // expression is the value that was assigned)
-            System.out.println("obj " + (obj == result ? "==" : "!=") +
-                               " result");
-
-            // Should print "obj.a == 1"
-            System.out.println("obj.a == " + obj.get("a", obj));
-
-            Scriptable b = (Scriptable) obj.get("b", obj);
-
-            // Should print "obj.b[0] == x"
-            System.out.println("obj.b[0] == " + b.get(0, b));
-
-            // Should print "obj.b[1] == y"
-            System.out.println("obj.b[1] == " + b.get(1, b));
-
-            // Should print {a:1, b:["x", "y"]}
-            Function fn = (Function) ScriptableObject.getProperty(obj, "toString");
-            System.out.println(fn.call(cx, scope, obj, new Object[0]));
         } finally {
             Context.exit();
         }
